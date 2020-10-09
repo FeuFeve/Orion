d78128174f3606d453f5575e1e867584473cdb2b
FeuFeve <clement-potin@outlook.com>
2020-10-09 13:06:04 +0200

    Initialize the project

---
+++ src/sample/Main.java
@@ -0,0 +1,45 @@
+package sample;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Writer writer = new FileWriter("test.json");
+
+//        TestObject testObject = new TestObject(2, 5);
+
+//        gson.toJson(testObject, writer);
+
+        writer.flush();
+        writer.close();
+
+//        TestObject testObject = gson.fromJson(new FileReader("test.json"), TestObject.class);
+//        System.out.println(testObject);
+
+        System.out.println("Done");
+        
+        launch(args);
+    }
+}
