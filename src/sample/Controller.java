f61768dcade2f56145f72a389a9cf60d434185ae
FeuFeve <clement-potin@outlook.com>
2020-10-09 16:12:45 +0200

    Add libraries and auto-completing text field for commands

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import org.controlsfx.control.textfield.TextFields;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField commandTextField;
+
+    String[] commands = {
+            "Build house <amount>",
+            "Build farm <amount>",
+            "Build granary <amount>",
+            "Add job builder <amount>"
+    };
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Initialization done.");
+        TextFields.bindAutoCompletion(commandTextField, commands);
+    }
 }
