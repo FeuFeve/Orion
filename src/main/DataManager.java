fbdb6f978c3b625c0654fd7764c6adabbce221b9
FeuFeve <clement-potin@outlook.com>
2020-10-12 15:02:14 +0200

    Add the DataManager and Game classes

---
+++ src/main/DataManager.java
@@ -0,0 +1,31 @@
+package main;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import models.Game;
+
+import java.io.*;
+
+public class DataManager {
+
+    static Game currentGame;
+
+    static void save(String saveName) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Writer writer = new FileWriter("saves/" + saveName + ".json");
+
+        gson.toJson(currentGame, writer);
+
+        writer.flush();
+        writer.close();
+
+        System.out.println("Game saved");
+    }
+
+    static void load(String saveName) throws FileNotFoundException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        currentGame = gson.fromJson(new FileReader("saves/" + saveName + ".json"), Game.class);
+
+        System.out.println("Game loaded");
+    }
+}
--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,45 +1,37 @@
 package main;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 
 public class Launcher extends Application {
 
+    public static Stage window;
+    public static Scene mainMenuScene, gameScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/game_view.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        window = primaryStage;
+
+        mainMenuScene = new Scene(FXMLLoader.load(getClass().getResource("../views/main_menu_view.fxml")));
+        gameScene = new Scene(FXMLLoader.load(getClass().getResource("../views/game_view.fxml")));
+
+        window.setTitle("Orion");
+        window.setScene(mainMenuScene);
+        window.setResizable(false);
+        window.show();
     }
 
 
     public static void main(String[] args) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//        Writer writer = new FileWriter("config/commands.json");
-
-//        TestObject testObject = new TestObject(2, 5);
-
-//        gson.toJson(testObject, writer);
-
-//        writer.flush();
-//        writer.close();
-
-//        TestObject testObject = gson.fromJson(new FileReader("test.json"), TestObject.class);
-//        System.out.println(testObject);
-
+        System.out.print("Initializing CommandManager...");
         CommandManager.init();
-        System.out.println("Done");
+        System.out.println(" Done");
 
+        System.out.println("Starting the game");
         launch(args);
     }
 }
