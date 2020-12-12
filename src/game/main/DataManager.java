97eac14109bf8d9eb24b5ae4b8d84af602d0e970
FeuFeve <clement-potin@outlook.com>
2020-12-12 19:10:04 +0100

    Add male/female first/last names config files, add a dev method to easily add new names and sort the configs

--- src/game/main/DataManager.java
+++ src/game/main/DataManager.java
@@ -1,5 +1,6 @@
 package game.main;
 
+import com.google.gson.reflect.TypeToken;
 import game.models.Game;
 import game.utilities.Chronometer;
 import game.utilities.Date;
@@ -7,6 +8,10 @@ import game.utilities.FileManager;
 
 import javax.swing.*;
 import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DataManager {
 
@@ -81,5 +86,66 @@ public class DataManager {
         chrono.stop();
         System.out.println(" Done in " + chrono.getDurationMsTxt());
         return 1;
+    }
+
+    // To add new names to the male/female first/last names config files
+    public static void main(String[] args) {
+        addToNames("female", "Edmonda Nellwyn Edith Valora Kaelyn ");
+        addToNames("last", "Priestleye Harlande Burkee Ewart Sweete");
+    }
+
+    /**
+     * Add names to one of the names.json file in the configs.
+     * Exemples:
+     * addToNames("female", "Ana Mia Rachel")
+     * addToNames("last", "Holmes Dickson")
+     * @param nameType  Type of the names to add ("male", "female" or "last)
+     * @param names     String of names separated by a space
+     */
+    private static void addToNames(String nameType, String names) {
+        System.out.println("******************************");
+
+        final String NAME_CONFIG_FOLDER = "config/Names/";
+        String filePath;
+        switch (nameType) {
+            case "male": filePath = NAME_CONFIG_FOLDER + "male first names.json"; break;
+            case "female": filePath = NAME_CONFIG_FOLDER + "female first names.json"; break;
+            case "last": filePath = NAME_CONFIG_FOLDER + "last names.json"; break;
+            default: System.out.println(nameType + " does not correspond to any referenced type."); return;
+        }
+
+        System.out.println("Saving " + nameType + " names...");
+
+        Type type = new TypeToken<ArrayList<String>>(){}.getType();
+        List<String> list = (List<String>) FileManager.loadArrayFromJson(filePath, type);
+
+        String[] namesArray = names.split(" ");
+        List<String> alreadyInList = new ArrayList<>();
+        int count = 0;
+        for (String name : namesArray) {
+            if (!name.isEmpty()) {
+                String stripedName = name.strip();
+                if (!list.contains(stripedName)) {
+                    list.add(stripedName);
+                    count++;
+                }
+                else {
+                    alreadyInList.add(stripedName);
+                }
+            }
+        }
+
+        if (!alreadyInList.isEmpty()) {
+            System.out.println(alreadyInList + " were already in the list.");
+        }
+
+        Collections.sort(list);
+        System.out.println("Added " + count + "/" + namesArray.length + " names.");
+        System.out.println("List: " + list);
+        System.out.println("Total: " + list.size());
+        FileManager.saveToJson(list, filePath);
+
+        System.out.println("Done.");
+        System.out.println("******************************\n");
     }
 }
